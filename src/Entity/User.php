feb7272204e069fbec9b0e9a554ae31cc2c76a2d
Djamila <djamilasmain1177@gmail.com>
2021-09-24 23:56:21 +0200

    integration categorie

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,10 +7,11 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="Cet email n'est pas valide")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -23,6 +24,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Assert\NotBlank(message="Vous devez ajouter un email")
      */
     private $email;
 
@@ -39,11 +41,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Vous devez ajouter votre prénom")
      */
     private $prenom;
 
     /**
      * @ORM\Column(type="string", length=255)
+     *  * @Assert\NotBlank(message="Vous devez ajouter votre nom de famille")   
      */
     private $nomDeFamille;
 
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,10 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,18 +20,67 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('prenom')
-            ->add('nomDeFamille')
-            ->add('email')
-            ->add('plainPassword', PasswordType::class, [
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre prénom'
+                ],
+                'required' => false
+            ])
+            ->add('nomDeFamille', TextType::class, [
+                'label' => 'Nom de famille',
+                'attr' => [
+                    'placeholder' => 'Entrez votre prénom'
+                ],
+                'required' => false
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'Entrez votre adresse email'
+                ],
+                // pour mettre mes contraintes de validations j'ajoute
+                'required' => false,
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'label' => 'Veuillez accepter les CGUV',
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'vous devez accepter les CGUV',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+
+                // j'annonce le type à repeter
+                'type' => PasswordType::class,
                 'mapped' => false,
+                'required' => false,
                 'attr' => ['autocomplete' => 'new-password'],
+                'invalid_message' => 'Les mots de passe ne correspondent pas',
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Vous devez entrer un mot de passe',
+                        ]),
+
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez votre mot de passe',
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Vous devez entrer une confirmation du mot de passe',
+                        ]),
+                    ],
+                ],
+
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
+
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
